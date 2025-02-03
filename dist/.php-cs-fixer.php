c4a11fa6c065b6a6dbc3d7a19e27efbcdbf6a242
Paul Tibbetts <paul.tibbetts@stickee.co.uk>
2025-02-03 17:08:28 +0000

    fix: missing distributable config

---
+++ dist/.php-cs-fixer.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+use Stickee\PhpCsFixerConfig;
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__ . '/app')
+    ->in(__DIR__ . '/config')
+    ->in(__DIR__ . '/routes')
+    ->in(__DIR__ . '/tests')
+    ->append([
+        __DIR__ . '/.php-cs-fixer.php',
+    ]);
+
+$overrideRules = [
+    'ordered_class_elements' => ['order' => ['use_trait']],
+];
+
+$config = PhpCsFixerConfig\Factory::fromRuleSet(
+    new PhpCsFixerConfig\RuleSet\Php83(),
+    $overrideRules
+);
+
+$config
+    ->setFinder($finder)
+    ->setCacheFile(__DIR__ . '/.php-cs-fixer.cache');
+
+$config->setParallelConfig(ParallelConfigFactory::detect());
+
+return $config;
